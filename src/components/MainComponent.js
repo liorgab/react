6fc5abbbe8ebdf4b5a3e00e3750be765f6edfb06
lior gabay <liorgab@gmail.com>
2018-12-19 16:16:10 +0200

    asignment 2 react router and single page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent'; 
 import { DISHES } from '../shared/dishes';
@@ -48,7 +49,8 @@ class Main extends Component {
         <Switch>
             <Route path="/home" component={HomePage} />
             <Route exact path="/contactus" component={Contact} />
-            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>} />
+            <Route exact path="/menu"    component={() => <Menu dishes={this.state.dishes}/>} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Redirect to="/home" />
         </Switch>
